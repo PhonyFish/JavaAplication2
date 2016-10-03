3f953f0537858761ca86285dad8bfba40492612f
PhonyFish <leisercom@gmail.com>
2016-10-03 10:55:25 -0500

    cambio de texto

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,8 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo!!");
+        System.out.println("Holis mundo!!");
+        System.out.println("lol lol lol lol lol");
     }
     
 }
